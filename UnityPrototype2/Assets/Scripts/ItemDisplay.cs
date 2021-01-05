d575328cc2f7c465aa176b02ef13d01467ba4158
jw026 <jesperwestlund@hotmail.com>
2021-01-05 14:16:59 +0100

    Itemdisplays, inventory

---
+++ UnityPrototype2/Assets/Scripts/ItemDisplay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ItemDisplay : MonoBehaviour
+{
+    [SerializeField] TMP_Text itemName;
+    private Item item;
+    public Item Item
+
+    {
+
+        set
+        {
+            item = value;
+            itemName.text = item.name;
+
+        }
+        get => item;
+
+    }
+}
+
+
+
--- UnityPrototype2/Assets/Scripts/UI_Inventory.cs
+++ UnityPrototype2/Assets/Scripts/UI_Inventory.cs
@@ -4,13 +4,59 @@ using UnityEngine;
 
 public class UI_Inventory : MonoBehaviour
 {
+    private bool open = false;
     private Inventory inventory;
+    [SerializeField] ItemDisplay itemDisplay;
+    [SerializeField] ItemDisplay itemDisplay2;
+    [SerializeField] GameObject uIInventoryMenu;
+    List<ItemDisplay> itemDisplays;
 
+
+    public void Start()
+    {
+        CloseInventory();
+        itemDisplay.gameObject.SetActive(false);
+        itemDisplay2.gameObject.SetActive(false);
+    }
     public void SetInventory(Inventory inventory)
     {
         this.inventory = inventory;
-        
+
     }
-        
-   
+
+    public void Update()
+    {
+        if (
+            Input.GetButtonDown("OpenInventory"))
+        {
+            if (open) CloseInventory();
+            else OpenInventory();
+        }
+
+    }
+    public void OpenInventory()
+    {
+        uIInventoryMenu.SetActive(true);
+        open = true;
+        itemDisplays = new List<ItemDisplay>();
+        for (int i = 0; i < inventory.itemList.Count; i++)
+        {
+            ItemDisplay newItemDisplay = Instantiate(itemDisplay);
+            newItemDisplay.transform.position = (itemDisplay.transform.position - itemDisplay2.transform.position) * i;
+            newItemDisplay.gameObject.SetActive(true);
+
+        }
+
+    }
+
+    public void CloseInventory()
+    {
+        uIInventoryMenu.SetActive(false);
+        open = false;
+        foreach (ItemDisplay item in itemDisplays)
+        {
+            Destroy(item.gameObject);
+        }
+    }
+
 }
