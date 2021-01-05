dfaa3685886506681e27d306ec0e85bb4e5ce5b5
jw026 <jesperwestlund@hotmail.com>
2021-01-05 12:12:50 +0100

    Inventory

---
+++ UnityPrototype2/Assets/Scripts/Inventory.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory
+{
+
+    private List<Item> itemList;
+
+    public Inventory()
+    {
+        itemList = new List<Item>();
+
+        
+        Debug.Log(itemList.Count);
+    }
+
+    public void AddItem(Item item)
+    {
+        itemList.Add(item);
+    }
+}
